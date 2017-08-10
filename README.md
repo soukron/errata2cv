5fe3191ff677e41a017b366d08095a75ac7a2a01
Sergio G <soukron@gmbros.net>
2017-08-10 02:37:36 +0200

    Initial 1.0.0 version

---
+++ README.md
@@ -0,0 +1,98 @@
+# errata2cv
+`errata2cv`is a python script to update your Content Views by injecting erratas, propagate them
+to affected Composite Content Views and/or update hosts	where the erratas are
+applicable.
+
+If you want a more complex handling of your Content Views there are other options to consider like
+[cvmanager](https://github.com/RedHatSatellite/katello-cvmanager) and/or [katello-publish-cv](https://github.com/RedHatSatellite/katello-publish-cvs) but that tools publish new Content View versions everytime, which may be overkill in some situations.
+
+## Features
+* Update Content Views using incremental versions.
+* Add erratas by filtering their type, severity and date.
+* Propagate erratas to Composite Content Views.
+* Apply erratas to hosts if applicable using Remote Execution job (this allows to re-run the job in hosts where it failed for any reason).
+* Import as a Job Template to schedule it easily.
+
+## Requisites
+* Tested and works on:Satellite 6.2. 
+* It only updates Content Views in _Library_ lifecycle environment. See **Examples** below.
+
+## Instructions
+Use command help to understand the arguments:
+
+~~~
+usage: errata2cv.py [-h] --cv CV [--type TYPE] [--severity SEVERITY]
+                    [--from-date FROM_DATE] [--to-date TO_DATE] [--propagate]
+                    [--update-hosts UPDATE_HOSTS] [--dry-run] [-v]
+
+Satellite 6 - Content View Errata Updater
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --cv CV               Comma-separated list of Content View names to update.
+  --type TYPE           Comma-separated list of errata types to include
+                        (bugfix, enhancement or security). Default: Security.
+  --severity SEVERITY   Comma-separated list of errata severity level to
+                        include (critical, important, moderate or low).
+                        Default: Critical.
+  --from-date FROM_DATE
+                        Date to use as a referente instead of Content View
+                        publishing date (YYYY/MM/DD).
+  --to-date TO_DATE     Date to use as a referente to stop including erratas
+                        (YYYY/MM/DD).
+  --propagate           Propagate incremental version to Composite Content
+                        Views. Default: False.
+  --update-hosts UPDATE_HOSTS
+                        Comma-separated list of lifecycle environments to
+                        update hosts with the included erratas.
+  --dry-run             Check for erratas but don't update Content Views nor
+                        update hosts.
+  -v, --version         show program's version number and exit
+~~~
+
+## Examples
+### Scenario
+`errata2cv` is specially useful where Composite Content Views are used and erratas must be updated frequently from base Content Views without updating other packages.
+
+Here is a sample Content View organization:
+  * **cv-rhel73-rpms**: simple Content View with RHEL 7.3 and Satellite 6.2 tools repositories. Their versions only promote to _Library_.
+  * **cv-rhel7-eap7-rpms**: simple Content View with EAP for RHEL 7 repositories. Their versions only promote to _Library_.
+  * **cv-rhel7-custom-rpms**: simple Content View with custom software for RHEL 7 repositories. Their versions only promote to _Library_.
+  * **ccv-rhel73-server**: Composite Content View with RHEL 7.3, EAP7 and Custom software Content Views. 
+    * Their version promote through a _DEV_ --> _INT_ --> _PRE_ --> _PRO_ path.
+
+### Examples
+These a different example situations you can resolve with `errata2cv`:
+* Update RHEL 7.3 Content View with Security - Critical erratas since its last publishing date:
+  ~~~
+  ./errata2cv.py --cv cv-rhel73-rpms
+  ~~~
+* Update RHEL 7.3 Content View with any Critical errata since its last publishing date and propagate to Composite Content View:
+   ~~~
+  ./errata2cv.py --cv cv-rhel73-rpms \
+                  --type bugfix,security,enhacement \
+                  --propagate
+  ~~~
+* Update RHEL 7.3 and EAP7 Content Views with any Security errata published since January 1st, 2017:
+  ~~~
+  ./errata2cv.py --cv cv-rhel73-rpms,cv-rhel7-eap7-rpms \
+                 --severity none,low,moderate,important,critical \
+                 --from-date 2017/01/01
+  ~~~
+* Update RHEL 7.3 Content View with any Security errata published since January 1st and update any host in _DEV_, _INT_ and _PRE_ lifecycle environment:
+  ~~~
+  ./errata2cv.py --cv cv-rhel73-rpms \
+                 --severity none,low,moderate,important,critical \
+                 --from-date 2017/01/01 \
+                 --propagate \
+                 --update-hosts DEV,INT,PRE
+  ~~~
+
+## TODO
+* Improve error handling.
+* Allow updating Content Views in other environments different from _Library_.
+* Create aliases for type and/or severity groups like "**any**" which contains all posible values.
+
+## Contact
+Reach me in [Twitter](http://twitter.com/soukron) or email in soukron at gmbros.net
+
---
+++ errata2cv.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+import json
+import sys
+import time
+from datetime import datetime
+import logging
+import argparse
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Satellite information
+#TODO: create command line arguments to set this values
+URL = "https://satellite.default/"
+USERNAME = "admin"
+PASSWORD = "unix1234"
+ORG_NAME = "Karmalabs"
+
+# Logging level
+#TODO: create a command line argument to set this value
+LOGGING_LEVEL = logging.INFO
+
+# API information
+SATELLITE_API = URL + "api/v2/"
+KATELLO_API = URL + "katello/api/v2/"
+TASKS_API = URL + "foreman_tasks/api/"
+POST_HEADERS = {'content-type': 'application/json'}
+SSL_VERIFY = False
+
+# Helper functions for GET/POST API methods
+#TODO: use some library to improve error handling/logging
+def get_json(location, json_data = ""):
+    logging.debug("Request: GET %s" % location)
+    if json_data: logging.debug("Request data: " + json.dumps(json_data, indent = 1))
+    result = requests.get(location,
+                            data = json_data,
+                            auth = (USERNAME, PASSWORD),
+                            verify = SSL_VERIFY)
+    logging.debug("Request result: " + json.dumps(result.json(), indent = 1))
+    return result.json()
+
+def post_json(location, json_data):
+    logging.debug("Request: POST %s" % location)
+    if json_data: logging.debug("Request data: " + json.dumps(json_data, indent = 1))
+    result = requests.post(location,
+                            data = json_data,
+                            auth = (USERNAME, PASSWORD),
+                            verify = SSL_VERIFY,
+                            headers = POST_HEADERS)
+    logging.debug("Request result: " + json.dumps(result.json(), indent = 1))
+    return result.json()
+
+def main():
+    # Read arguments from command line
+    parser = argparse.ArgumentParser(description = "Satellite 6 - Content View Errata Updater")
+    parser.add_argument("--cv", help = "Comma-separated list of Content View names to update.", required = True)
+    parser.add_argument("--type", type = str.lower, help = "Comma-separated list of errata types to include (bugfix, enhancement or security). Default: Security.", default = "security")
+    parser.add_argument("--severity", type = str.lower, help = "Comma-separated list of errata severity level to include (critical, important, moderate or low). Default: Critical.", default = "critical")
+    parser.add_argument("--from-date", help = "Date to use as a referente instead of Content View publishing date (YYYY/MM/DD).", default = "")
+    parser.add_argument("--to-date", help = "Date to use as a referente to stop including erratas (YYYY/MM/DD).", default = "")
+    parser.add_argument("--propagate", action = "store_true", help = "Propagate incremental version to Composite Content Views. Default: False.", default = False)
+    parser.add_argument("--update-hosts", help = "Comma-separated list of lifecycle environments to update hosts with the included erratas.", default = "")
+    parser.add_argument("--dry-run", action = "store_true", help = "Check for erratas but don't update Content Views nor update hosts.", default = False)
+    parser.add_argument("-v", "--version", action = "version", version = "%(prog)s 1.0.0")
+    args = vars(parser.parse_args())
+
+    # Setup logging
+    log = logging.getLogger(__name__)
+    logging.basicConfig(level = LOGGING_LEVEL,
+                    stream = sys.stdout,
+                    format = "%(asctime)s %(levelname)s: %(message)s",
+                    handlers = [logging.StreamHandler()])
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+    # Get organization
+    logging.debug("Looking for organization information.")
+    org = get_json(KATELLO_API + "organizations/" + ORG_NAME)
+
+    # Compose search strings using input arguments
+    severity_search = "(severity = " + ' or severity = '.join([x.capitalize() for x in args["severity"].split(',')]) + ")"
+    type_search = "(type = " + ' or type = '.join(args["type"].split(',')) + ")"
+
+    # Loop over content-views to find any new errata in their repositories
+    for cv_name in args["cv"].split(","):
+        logging.info("Processing content-view %s." % cv_name)
+        errata_ids = []
+        try:
+            # Compose GET parameters to get given content view
+            get_params = {
+                "noncomposite": 1,
+                "nondefault": 1,
+                "search": "name=%s" % cv_name
+            }
+            cv = get_json(KATELLO_API + "organizations/%s/content_views" % org["id"], get_params)["results"][0]
+        except:
+            logging.warning("Skipping non existing content-view %s." % cv_name)
+            continue
+
+        # Calculate from-date acording to parameters and last version published
+        if args["from_date"] == "":
+            last_published = cv["last_published"]
+            if last_published is None:
+                last_published = "1970-01-01 00:00:00 UTC"
+            from_date = datetime.strptime(last_published, "%Y-%m-%d  %X %Z").strftime('%Y/%m/%d')
+        else:
+            from_date = args["from_date"]
+        logging.debug("Using %s as start date." % from_date)
+
+        # Compose GET parameters to search errata
+        get_params = {
+            "repository_id": '',
+            "paged": False,
+            "errata_restrict_applicable": False,
+            "errata_restrict_installable": False,
+            "search": "%s and %s and updated > '%s'" % (type_search, severity_search, from_date)
+        }
+
+        # Append to-date if parameter was provided
+        if args["to_date"]:
+            get_params ["search"] += " and updated < '%s'" % args["to_date"]
+            logging.debug("Using %s as end date." % from_date)
+
+        need_publish = False
+        # Get erratas matching criteria for each repository
+        for repo in cv["repositories"]:
+            logging.info("Searching for erratas in repository %s" % repo["name"])
+            get_params["repository_id"] = repo["id"]
+            errata_in_repo = get_json(KATELLO_API + "errata", get_params)
+
+            # Save errata id in an array and warn if any suggests a reboot
+            for errata in errata_in_repo["results"]:
+                logging.info("Found %s (%s - %s) errata. Reboot suggested: %s." % (errata["errata_id"], errata["type"].capitalize(), errata["severity"], "Yes" if errata["reboot_suggested"] else "No"))
+                errata_ids.append(errata["errata_id"])
+
+        # Publish incremental version if there are any errata in the array
+        errata_ids = list(set(errata_ids))
+        if len(errata_ids) > 0:
+            # Get CV version in Library environment only
+            for version in cv["versions"]:
+                if 1 in version["environment_ids"]:
+                    logging.info("Selected content-view %s (version %s) as baseline to include %s erratas." % (cv["name"], version["version"], len(errata_ids)))
+                else:
+                    logging.debug("Skipping content-view %s (version %s): Not in Library." % (cv["name"], version["version"]))
+
+            # Compose POST parameters to publish incremental version
+            post_params = {
+                "propagate_all_composites": 1 if args["propagate"] else 0,
+                "resolve_dependencies": 1,
+                "add_content": { "errata_ids": errata_ids },
+                "content_view_version_environments": [ {
+                    "content_view_version_id": version["id"],
+                    "environment_ids": [ 1 ]
+                } ]
+            }
+
+            # If no dry-run execution publish an incremental version and propagate it to all composite content views
+            if args["dry_run"] == False:
+                logging.info("Publishing incremental content-view version.")
+                incremental_update = post_json(KATELLO_API + "content_view_versions/incremental_update", json.dumps(post_params))
+
+                # Loop until task is finished
+                while(incremental_update["pending"] != False):
+                    logging.info("Waitting for publishing task to complete.")
+                    time.sleep(60)
+                    incremental_update = get_json(TASKS_API + "tasks/" + incremental_update["id"])
+
+                if incremental_update["result"] != "success":
+                    logging.error("Error publishing incremental content-view version. Skipping installation in hosts.")
+                    continue
+
+                # Apply erratas to hosts using remote execution "Install Errata - Katello SSH Default" job if environments were provided
+                if args["update_hosts"]:
+                    logging.info("Installing errata in hosts (if applicable).")
+
+                    # Compose search query for hosts in given lifecycle environments and erratas are applicable
+                    environments_search = "(lifecycle_environment=" + " or lifecycle_environment=".join(args["update_hosts"].split(",")) + ")"
+                    applicable_search = "(applicable_errata=" + " or applicable_errata=".join(errata_ids) + ")"
+                    search_query = environments_search + " and " + applicable_search
+
+                    # Get template id
+                    template_json = get_json(SATELLITE_API + 'job_templates', {"search": 'name = "Install Errata - Katello SSH Default"'})
+                    if len(template_json["results"]) > 0:
+                        template_id = template_json["results"][0]["id"]
+
+                        # Compose POST parameters to invoke the job
+                        post_params = {
+                            "job_invocation": {
+                                "job_template_id": template_id,
+                                "inputs": {
+                                    "errata": ",".join(errata_ids)
+                                },
+                                "search_query": search_query,
+                                "targeting_type": "static_query"
+                            }
+                        }
+
+                        # Invoke job execution and continue with another CV in the list (if any)
+                        job_execution = post_json(SATELLITE_API + 'job_invocations', json.dumps(post_params))
+                    else:
+                        logging.info("Remote execution job \"Install Errata - Katello SSH Default\" not found. Skipping errata installation.")
+                else:
+                    logging.debug("Skipping errata installation as no host lifecycle environments were provided.")
+            else:
+                logging.info("Skipping incremental content-view and/or installation in hosts as dry-run was specified.")
+            logging.info("Finished processing CV %s." % cv_name)
+        else:
+            logging.info("No new existing erratas for %s CV." % cv["name"])
+
+if __name__ == "__main__":
+    main()
